07c5f85126057ee39462bd6b6a6eac97fcb87e58
nisarga-m-ec <nisarga.m2027@gmail.com>
2026-06-30 12:15:30 +0530

    Create SwapNumbers.cpp

---
+++ Day-01/SwapNumbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    cout<<"After Swap: "<<a<<" "<<b;
+
+    return 0;
+}
