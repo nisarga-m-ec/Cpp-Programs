15b4adb3af56fffb912b2d845920afdc5fea0db3
nisarga-m-ec <nisarga.m2027@gmail.com>
2026-07-01 13:16:28 +0530

    Create AverageOfFive.cpp

---
+++ Day-02/AverageOfFive.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a,b,c,d,e,avg;
+
+    cout<<"Enter five numbers: ";
+    cin>>a>>b>>c>>d>>e;
+
+    avg=(a+b+c+d+e)/5;
+
+    cout<<"Average = "<<avg;
+
+    return 0;
+}
