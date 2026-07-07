86ace5bac1226ada6cc8c0f8a7759fd91c5ffd53
nisarga-m-ec <nisarga.m2027@gmail.com>
2026-07-07 12:45:34 +0530

    Create MultipleOf3And5.cpp

---
+++ Day-03/MultipleOf3And5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    if(num%3==0 && num%5==0)
+        cout<<"Multiple of both 3 and 5";
+    else
+        cout<<"Not a multiple of both 3 and 5";
+
+    return 0;
+}
