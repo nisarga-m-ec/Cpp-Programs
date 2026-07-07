0dd0df58ce1c3ebc49bdceabcbb4c3fda2298600
nisarga-m-ec <nisarga.m2027@gmail.com>
2026-07-07 12:41:27 +0530

    Create AbsoluteValue.cpp

---
+++ Day-03/AbsoluteValue.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    if(num<0)
+        num=-num;
+
+    cout<<"Absolute Value = "<<num;
+
+    return 0;
+}
